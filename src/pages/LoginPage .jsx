ef84f9d185226684643a484a88d8be8aa0a03a2d
DheerajRay-01 <dheerajray2003@gmail.com>
2025-04-09 12:42:30 +0530

    prevent user to go to login page when he logged in

--- src/pages/LoginPage .jsx
+++ src/pages/LoginPage .jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Login from '../components/Login.jsx'
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router';
 
 function LoginPage () {
-  return (
-    <div>
-        <Login/>
-    </div>
-  )
+const user = useSelector((state) => state.user?.user?.user);
+
+
+if (user) return <Navigate to="/" replace />;
+  
+return (
+  <div>
+    <Login />
+  </div>
+);
+
 }
 
 export default LoginPage 
